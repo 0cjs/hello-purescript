df35bfc5c95acbebf0949d0525f6c023c0e683a6
Nishant Rodrigues <nishantjr@gmail.com>
2016-06-22 22:45:16 +0900

    Add test for makeGreeting and sample Unit tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,8 +2,24 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Test.Unit.Main (runTest)
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit (suite, test)
+import Test.Unit.Assert as Assert
+
+import Main (makeGreeting)
+
+main :: forall t1.
+    Eff (console :: CONSOLE, testOutput :: TESTOUTPUT | t1) Unit
+main = runTest
+ do suite "sample tests"
+     do test "arithmetic"
+         do Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
+        test "more tests"
+         do Assert.equal (2 + 2) 4
+            Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal (2 + 2) 5
+    suite "Main"
+     do test "greeting"
+         do Assert.equal "Hello, sailor!" $ makeGreeting "sailor"
