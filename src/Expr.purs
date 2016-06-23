0bf3b0dc12ef1fd35df42eb3d3bd09114d027f39
Nishant Rodrigues <nishantjr@gmail.com>
2016-06-23 22:02:28 +0900

    Expr: extract method isPrefixOf

--- src/Expr.purs
+++ src/Expr.purs
@@ -1,5 +1,5 @@
 module Expr (
-    Expr(..), eval
+    Expr(..), eval, isPrefixOf
 ) where
 
 import Prelude
@@ -12,10 +12,13 @@ data Expr
 
 eval :: Expr -> String -> Prim.Boolean
 eval (Equal x) y        = x == y
-eval (Prefix xxs) yys   =
+eval (Prefix xxs) yys   = xxs `isPrefixOf` yys
+
+isPrefixOf :: String -> String -> Prim.Boolean
+isPrefixOf xxs yys =
     case uncons xxs, uncons yys of
         Nothing, _       -> true
         _,       Nothing -> false
         Just { head:x, tail:xs }, Just { head:y, tail:ys } ->
             if  x /= y  then false
-                        else eval (Prefix xs) ys
+                        else isPrefixOf xs ys
--- test/Expr.purs
+++ test/Expr.purs
@@ -9,7 +9,7 @@ import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 
-import Expr (Expr(..), eval)
+import Expr (Expr(..), eval, isPrefixOf)
 
 main :: forall t1.
     Eff (console :: CONSOLE, testOutput :: TESTOUTPUT | t1) Unit
@@ -17,11 +17,13 @@ main = runTest do
     suite "eval" do
         test "equal"        $ is   $ eval (Equal "abc") "abc"
         test "!equal"       $ aint $ eval (Equal "abc") "def"
-        test "prefix nothin"$ is   $ eval (Prefix "") ""
-        test "prefix short" $ aint $ eval (Prefix "abc") "ab"
-        test "prefix equal" $ is   $ eval (Prefix "abc") "abc"
         test "prefix long"  $ is   $ eval (Prefix "abc") "abcde"
-        test "prefix wrong" $ aint $ eval (Prefix "abx") "abcde"
+    suite "isPrefixOf" do
+        test "prefix nothin"$ is   $    "" `isPrefixOf`  ""
+        test "prefix short" $ aint $ "abc" `isPrefixOf`  "ab"
+        test "prefix equal" $ is   $ "abc" `isPrefixOf`  "abc"
+        test "prefix long"  $ is   $ "abc" `isPrefixOf`  "abcde"
+        test "prefix wrong" $ aint $ "abx" `isPrefixOf`  "abcde"
     where
         is   = Assert.equal true
         aint = Assert.equal false
