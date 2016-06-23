ce34d7cd5270079a6f4507cede3a166baa975267
Curt J. Sampson <cjs@cynic.net>
2016-06-23 13:20:27 +0900

    Expr: Fix `eval` of Prefix op

--- src/Expr.purs
+++ src/Expr.purs
@@ -10,21 +10,10 @@ data Expr
 
 eval :: Expr -> String -> Prim.Boolean
 eval (Equal x) y        = x == y
-eval (Prefix "") _      = true
-eval (Prefix _) ""      = false
-{- All I want is...
-    eval (Prefix (x:xs)) (y:ys) =
-        | x /= y    = false
-        | otherwise = eval (Prefix xs) ys 
--}
 eval (Prefix xxs) yys   =
-    case uncons xxs of
-        Nothing -> false
-        Just { head:x, tail:xs } ->
-            | x /= y    = false
-            | otherwise = eval (Prefix xs) ys
-    where
-        { y:Char, ys:String } = case uncons yys of
-            Nothing  -> { '\0', "" }
-            Just yys -> yys
-
+    case uncons xxs, uncons yys of
+        Nothing, _       -> true
+        _,       Nothing -> false
+        Just { head:x, tail:xs }, Just { head:y, tail:ys } ->
+            if  x /= y  then false
+                        else eval (Prefix xs) ys
